f1fa1ff3b4242b96b2c8ca8a8bc7de4811d476aa
MD Rajob Ali <119127566+RajobAli@users.noreply.github.com>
2023-01-24 20:29:02 +0600

    Sum

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+var d = document.querySelector(".button");
+d.addEventListener('click',()=> {
+var a = document.querySelector(".container");
+var b =parseInt(document.querySelector(".base").value);
+var c =parseInt(document.querySelector(".height").value);
+
+var e = document.querySelector("h1");
+var f = document.querySelector("h2");
+
+
+
+document.write(b+c);
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+});
